75a9ab59e2d236f7d579b9a57d10bc839dcfece0
matthcol <matthias.colin@gmail.com>
2025-03-05 10:40:33 +0100

    fonction returning None

--- fonctions.py
+++ fonctions.py
@@ -1,4 +1,10 @@
 def code_city(city: str, department: int, counter: int) -> str:
     a = city[:3].upper()
-    # return f"{a}{counter}D{department}"
     return f"{a:_<3}{counter:05d}D{department:03d}"
+
+code1 = code_city('Pau', 64, 1)
+print(code1)
+
+# appel erroné en terme de type
+code2 = code_city('Pau', 64.5, 1) 
+# print(code2)
